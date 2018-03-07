cc329d35638259d9a9f89b9b2504670442860bf7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-03-07 09:55:26 +0800

    Initial import of some CSS styles

---
+++ CONTRIBUTING.md
@@ -0,0 +1,7 @@
+# Developer notes
+
+Files of interest:
+
+- [Home page](src/pages/index.js)
+- [Layout](src/layouts/index.js)
+- [CSS](src/layouts/index.css)
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import 'sanitize.css'
-import './index.css'
+import '../styles/index.css'
 
 /**
  * Base layout
