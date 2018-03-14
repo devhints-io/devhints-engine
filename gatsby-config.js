0917da3639111dec873e924da48f51bec1623e08
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-03-14 12:59:17 +0800

    Move gatsby config to /gatsby-*.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,1 +1,41 @@
-require('./src/gatsby/gatsby-config.js')(__dirname, module)
+const root = require('path').resolve.bind(null, __dirname)
+
+/*
+ * Where cheatsheets are
+ */
+
+const SHEET_PATH = process.env.SHEET_PATH || root('sheets')
+
+/*
+ * Gatsby configuration
+ */
+
+module.exports = {
+  // Enable this for temp GitHub pages deployment
+  // pathPrefix: '/cheatsheets-2018',
+  siteMetadata: {
+    title: 'Devhints'
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: root('src/cms/cms.js')
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: SHEET_PATH,
+        name: 'markdown-pages'
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: ['gatsby-remark-component']
+      }
+    }
+  ]
+}
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,65 @@
-require('./src/gatsby/gatsby-node.js')(__dirname, module)
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See:
+ *
+ * - https://www.gatsbyjs.org/docs/node-apis/
+ * - https://www.gatsbyjs.org/docs/adding-markdown-pages/#create-static-pages-using-gatsbys-node-api
+ */
+
+const root = require('path').resolve.bind(null, __dirname)
+
+/**
+  * Create pages.
+  */
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  const SheetTemplate = root('src/templates/SheetTemplate.js')
+
+  return graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: SheetTemplate,
+        context: {} // additional data can be passed via context
+      })
+    })
+  })
+}
+
+/**
+  * Modify Webpack configuration.
+  */
+
+exports.modifyWebpackConfig = ({ config }) => {
+  const noop = root('src/lib/helpers/noop.js')
+
+  // isotope-layout tries to require('jquery'), but let's let that
+  // fail silently. We don't want it to load jQuery.
+  config.merge({
+    resolve: {
+      alias: {
+        jquery: noop,
+        jsdom: noop
+      }
+    }
+  })
+}
