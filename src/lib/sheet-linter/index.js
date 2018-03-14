52ae4899d7bd0f57deb89fafc65099a88418fc90
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-03-14 13:18:38 +0800

    Initial implementation of sheet-linter

---
+++ src/lib/sheet-linter/index.js
@@ -0,0 +1,45 @@
+/* Lint our sheets */
+/* TODO sheet linter */
+const fastmatter = require('fastmatter')
+const fs = require('fs')
+const { resolve } = require('path')
+const concat = require('concat-stream')
+const yaml = require('js-yaml')
+
+const fname = resolve(__dirname, '../../../sheets/vim.md')
+
+/**
+ * Read a file into `{meta, body}`
+ */
+
+function read (fname) {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(fname).pipe(
+      fastmatter.stream(function (meta) {
+        this.pipe(concat((body) => {
+          // may need to body.toString() this
+          resolve({ meta, body: body.toString() })
+        }))
+      })
+    )
+  })
+}
+
+/**
+ * Serialize a `{meta, body}` into a file.
+ */
+
+function serialize({ meta, body }) {
+  return `---\n${yaml.safeDump(meta).trim()}\n---\n${body}`
+}
+
+/*
+ * Run
+ */
+
+read(fname)
+  .then(result => {
+    // console.log(require('util').inspect(result))
+    const file = serialize(result)
+    console.log(file)
+  })
