fa9d15c1dbb337acc72ab3c234cad38ebafb2545
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-04-18 11:14:01 +0800

    rehype-table-separators: Add more stub functions
    Still doesn't work, but now we know where to work on the table body.

--- packages/rehype-table-separators/__tests__/index.test.js
+++ packages/rehype-table-separators/__tests__/index.test.js
@@ -1,6 +1,55 @@
+/* @jsx hJsx */
 /* eslint-env jest */
 import apply from '../index'
 
+/* eslint-disable no-unused-vars */
+// TODO: move these helpers into a separate place
+import hJsx from '../../rehype-decorate/lib/helpers/h_jsx'
+import render from '../../rehype-decorate/lib/helpers/hast_to_react'
+/* eslint-enable no-unused-vars */
+import React from 'react'
+
 it('exports a function', () => {
   expect(typeof apply).toEqual('function')
+})
+
+it('works', () => {
+  const input = (
+    <table>
+      <tbody>
+        <tr>
+          <td>Hello</td>
+          <td>World</td>
+        </tr>
+        <tr>
+          <td>---</td>
+          <td>---</td>
+        </tr>
+        <tr>
+          <td>Hola</td>
+          <td>Mundo</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+
+  const expected = (
+    <table>
+      <tbody>
+        <tr>
+          <td>Hello</td>
+          <td>World</td>
+        </tr>
+      </tbody>
+      <tbody>
+        <tr>
+          <td>Hola</td>
+          <td>Mundo</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+
+  const output = apply(input)
+  expect(render(output)).toEqual(render(expected))
 })
--- packages/rehype-table-separators/index.js
+++ packages/rehype-table-separators/index.js
@@ -2,15 +2,55 @@ import map from 'unist-util-map'
 
 /**
  * Fixes table separators in an HAST node.
- * @param {Node} root The root node.
- * @param {Options} [options] The options.
  * @returns {Node} a copy of the root node, but with tables fixed.
+ *
+ * @param {Node} root The root node
+ * @param {Options} [options] The options
  */
 
 function apply (root, options) {
   return map(root, node => {
-    return node
+    if (node.tagName === 'table') {
+      return mapTable(node, options)
+    } else {
+      return node
+    }
   })
 }
+
+/**
+ * Maps a table, lol.
+ *
+ * @param {Node} node The table node
+ * @param {Options} [options] Options
+ * @returns {Node} a copy of the table that's been processed.
+ * @private
+ */
+
+export function mapTable (table, options) {
+  const children = table.children.reduce((children, child) => {
+    if (child.tagName === 'tbody') {
+      return [...children, ...reduceTBody(child, options)]
+    } else {
+      return [...children, child]
+    }
+  }, [])
+
+  return { ...table, children }
+}
+
+/**
+ * TBD
+ * @private
+ */
+
+export function reduceTBody (tbody, options) {
+  // TODO break apart
+  return [tbody]
+}
+
+/*
+ * Export
+ */
 
 export default apply
