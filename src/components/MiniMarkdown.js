8bd4191487323f38579de7bcd48ee0751e8993d1
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-05-16 06:07:45 +0800

    Implement mini-markdown (#3)

--- src/components/MiniMarkdown.js
+++ src/components/MiniMarkdown.js
@@ -7,14 +7,58 @@ export type Props = {
 }
 
 export const MiniMarkdown = ({ source }: Props) => (
-  <React.Fragment>
-    <p>{markdownish(source)}</p>
-  </React.Fragment>
+  <span
+    className='mini-markdown'
+    dangerouslySetInnerHTML={{ __html: paragraphify(markdownish(source)) }}
+  />
 )
 
+/**
+ * A very simply markdown-to-HTML converter, but only works with a very small
+ * subset of inline elements. Returns HTML.
+ */
+
 export function markdownish (source: string) {
-  // TODO mini-markdown
-  return [source]
+  return (
+    source
+      // Code
+      .replace(
+        /`(.*?)`/g,
+        (_, label: string) => `<code>${markdownish(label)}</code>`
+      )
+      // Bold
+      .replace(
+        /\*\*(.*?)\*\*/g,
+        (_, label: string) => `<strong>${markdownish(label)}</strong>`
+      )
+      // Link
+      .replace(
+        /\[([^\]]*)\]\((.*?)\)/g,
+        (_, label: string, url: string) =>
+          `<a href=${s(url)}>${markdownish(label)}</a>`
+      )
+  )
+}
+
+/**
+ * Splits paragraph sections into `<p>` elements.
+ * Returns HTML.
+ */
+
+export function paragraphify (source: string) {
+  return source
+    .split('\n\n')
+    .map(para => `<p>${para}</p>`)
+    .join('')
+}
+
+/**
+ * Quotes a string
+ * @private
+ */
+
+function s (str: string) {
+  return JSON.stringify(str)
 }
 
 export default MiniMarkdown
