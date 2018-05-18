94571634c283fff18c8529fdf56eb1337913987c
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-05-18 20:48:58 +0800

    Fix 404 page not updating

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,26 +4,47 @@ import Link from 'gatsby-link'
 import { CONTENT } from '../../config'
 import ExternalSearchLinks from '../components/ExternalSearchLinks'
 
-export type Props = {
-  location: {
-    pathname: string // => '/404'
-  }
-}
+export type Props = {|
+  keyword: ?string, // => 'rails' | null
+  title: string, // => 'Not Found'
+  description: string, // => 'Try searching!'
+  home: string // => 'Back to home'
+|}
 
 /**
  * The 404 page.
  */
 
-const NotFoundPage = (props: Props) => {
-  const keyword = keywordify(props.location.pathname)
-  const content = CONTENT.notFound || {}
+export const NotFoundPage = () => {
+  const pathname = window && window.location && window.location.pathname
+  const keyword = keywordify(pathname)
 
   return (
+    <NotFoundPageView
+      keyword={keyword}
+      title={CONTENT.notFound.notFound}
+      description={CONTENT.notFound.description}
+      home={CONTENT.notFound.home}
+    />
+  )
+}
+
+/**
+ * The 404 page view.
+ */
+
+export const NotFoundPageView = ({
+  keyword,
+  title,
+  description,
+  home
+}: Props) => {
+  return (
     <div>
-      <h1>{content.notFound}</h1>
-      <p>{content.description}</p>
-      <ExternalSearchLinks keyword={keyword} />
-      <Link to='/'>{content.home}</Link>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      {keyword && <ExternalSearchLinks keyword={keyword} />}
+      <Link to='/'>{home}</Link>
     </div>
   )
 }
@@ -32,14 +53,19 @@ const NotFoundPage = (props: Props) => {
  * Convert pathname to keywords.
  *
  *     keywordify('/gatsby') => 'gatsby'
- *     keywordify('/rails/models') => 'rails-models'
+ *     keywordify('/rails/models.html') => 'rails-models'
+ *     keywordify() => undefined
  */
 
-function keywordify (str: string): string {
+function keywordify (str?: string): ?string {
+  if (!str) return
+
   return str
     .slice(1)
     .replace(/\//g, '-')
     .replace(/ /g, '_')
+    .replace(/\.html$/, '')
+    .replace(/^cheatsheets-ng\//, '')
 }
 
 export default NotFoundPage
