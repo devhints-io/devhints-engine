d36166634724820d8f08c70df714f37788b21af5
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-08-03 07:03:06 +0800

    Finally begin implementing icons from Clarity

---
+++ packages/devhints-icons/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/no-webpack-loader-syntax */
+
+export { default as homeLine } from '!raw-loader!./icons/home-line.svg'
--- src/components/HomeButton.js
+++ src/components/HomeButton.js
@@ -1,6 +1,7 @@
 /* @flow */
 import * as React from 'react'
 import Link from 'gatsby-link'
+import { homeLine } from 'devhints-icons'
 
 /**
  * Home button
@@ -8,7 +9,44 @@ import Link from 'gatsby-link'
 
 export const HomeButton = () => (
   <Link className='home-button' to='/'>
-    <i />
+    <i dangerouslySetInnerHTML={{ __html: homeLine }} />
+
+    <style jsx>{`
+      @import 'src/styles/common';
+
+      :global(.home-button) {
+        & {
+          display: inline-block;
+          box-shadow: inset 0 0 0 1px var(--dark-line-color);
+          border-radius: 50%;
+          padding: 12px;
+        }
+
+        &,
+        &:visited {
+          color: var(--text-mute);
+        }
+
+        &:hover,
+        &:focus {
+          background-color: var(--brand-a);
+          color: white;
+        }
+      }
+
+      i :global(svg) {
+        width: 24px;
+        height: 24px;
+      }
+
+      i :global(.clr-i-outline) {
+        fill: var(--dark-line-color);
+      }
+
+      :global(.home-button:hover) :global(.clr-i-outline) {
+        fill: white;
+      }
+    `}</style>
   </Link>
 )
 
