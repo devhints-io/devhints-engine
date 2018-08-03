7daa45fd670d2ca9d816511b378ba49911cf9db5
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-08-03 20:55:11 +0800

    Refactor CSS in some components

--- src/components/CommentsArea.js
+++ src/components/CommentsArea.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import CommentsAreaSummary from './CommentsAreaSummary'
 import CommentsSection from './CommentsSection'
+import css from 'styled-jsx/css'
 
 /**
  * Comments area
@@ -15,42 +16,52 @@ export const CommentsArea = () => (
         <CommentsSection />
       </details>
     </div>
-    <style jsx>{`
-      @import 'src/styles/common';
-
-      .comments-area {
-        margin: 32px 0 16px 0;
-        @apply --has-container;
-      }
-
-      .container {
-        @apply --gutter-padding-left;
-        @apply --gutter-padding-right;
-        max-width: var(--area-width);
-        margin: 0 auto;
-      }
-
-      /* Horizontal line */
-      & > .container::before {
-        content: '';
-        display: block;
-        background: linear-gradient(
-          to right,
-          var(--dark-line-color) 50%,
-          transparent
-        );
-        height: 1px;
-      }
-
-      details {
-        margin-bottom: -16px;
-      }
-
-      details[open] {
-        margin-bottom: 0;
-      }
-    `}</style>
+    <style jsx>{STYLE}</style>
   </section>
 )
+
+/*
+ * CSS
+ */
+
+export const STYLE = css`
+  @import 'src/styles/common';
+
+  .comments-area {
+    margin: 32px 0 16px 0;
+    @apply --has-container;
+  }
+
+  .container {
+    @apply --gutter-padding-left;
+    @apply --gutter-padding-right;
+    max-width: var(--area-width);
+    margin: 0 auto;
+  }
+
+  /* Horizontal line */
+  & > .container::before {
+    content: '';
+    display: block;
+    background: linear-gradient(
+      to right,
+      var(--dark-line-color) 50%,
+      transparent
+    );
+    height: 1px;
+  }
+
+  details {
+    margin-bottom: -16px;
+  }
+
+  details[open] {
+    margin-bottom: 0;
+  }
+`
+
+/*
+ * Export
+ */
 
 export default CommentsArea
--- src/components/CommentsAreaSummary.js
+++ src/components/CommentsAreaSummary.js
@@ -1,6 +1,7 @@
 /* @flow */
 import React from 'react'
 import { talkBubblesLine, talkBubblesSolid } from 'devhints-icons'
+import css from 'styled-jsx/css'
 
 /**
  * Summary in the comments area
@@ -19,82 +20,92 @@ export const CommentsAreaSummary = ({ count }: { count: number }) => (
     <strong className='count'>{count}</strong>{' '}
     <span className='suffix'>comments for this cheatsheet.</span>{' '}
     <span className='fauxlink'>Write yours!</span>
-    <style jsx>{`
-      @import 'src/styles/common';
-
-      /* Root component */
-      summary {
-        @apply --font-size-1;
-        color: var(--brand-a);
-        padding: 24px 0;
-        white-space: nowrap;
-        cursor: pointer;
-      }
-
-      summary:hover,
-      summary:focus {
-        &,
-        & > .suffix {
-          color: var(--brand-a7);
-        }
-
-        & > .fauxlink {
-          border-bottom: solid 1px var(--brand-a7);
-        }
-      }
-
-      .count {
-        font-weight: bold;
-      }
-
-      .count::before {
-        content: '';
-        vertical-align: middle;
-        color: var(--brand-a);
-        margin: 0 8px 0 0;
-      }
-
-      .suffix {
-        color: var(--text-mute);
-      }
-
-      .fauxlink {
-        margin-left: 4px;
-        border-bottom: solid 1px color(var(--brand-a) alpha(25%));
-      }
-
-      /* Icon */
-      .icon :global(svg) {
-        width: 24px;
-        height: 24px;
-      }
-
-      .icon :global(.clr-i-outline) {
-        fill: var(--brand-a);
-      }
-
-      /* Icon on non-hover */
-      .icon.-line {
-        display: inline;
-      }
-
-      .icon.-solid {
-        display: none;
-        margin-right: -4px;
-      }
-
-      /* Icon on hover */
-      summary:hover {
-        & .icon.-solid {
-          display: inline;
-        }
-
-        & .icon.-line {
-          display: none;
-        }
-      }
-    `}</style>
+    <style jsx>{STYLE}</style>
   </summary>
 )
+
+/*
+ * CSS
+ */
+
+export const STYLE = css`
+  @import 'src/styles/common';
+
+  /* Root component */
+  summary {
+    @apply --font-size-1;
+    color: var(--brand-a);
+    padding: 24px 0;
+    white-space: nowrap;
+    cursor: pointer;
+  }
+
+  summary:hover,
+  summary:focus {
+    &,
+    & > .suffix {
+      color: var(--brand-a7);
+    }
+
+    & > .fauxlink {
+      border-bottom: solid 1px var(--brand-a7);
+    }
+  }
+
+  .count {
+    font-weight: bold;
+  }
+
+  .count::before {
+    content: '';
+    vertical-align: middle;
+    color: var(--brand-a);
+    margin: 0 8px 0 0;
+  }
+
+  .suffix {
+    color: var(--text-mute);
+  }
+
+  .fauxlink {
+    margin-left: 4px;
+    border-bottom: solid 1px color(var(--brand-a) alpha(25%));
+  }
+
+  /* Icon */
+  .icon :global(svg) {
+    width: 24px;
+    height: 24px;
+  }
+
+  .icon :global(.clr-i-outline) {
+    fill: var(--brand-a);
+  }
+
+  /* Icon on non-hover */
+  .icon.-line {
+    display: inline;
+  }
+
+  .icon.-solid {
+    display: none;
+    margin-right: -4px;
+  }
+
+  /* Icon on hover */
+  summary:hover {
+    & .icon.-solid {
+      display: inline;
+    }
+
+    & .icon.-line {
+      display: none;
+    }
+  }
+`
+
+/*
+ * Export
+ */
 
 export default CommentsAreaSummary
