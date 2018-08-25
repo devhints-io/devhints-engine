8bf27441be5b1eb60ac0053fc3490dabff645b19
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-08-25 08:15:40 +0800

    Update formatting

--- src/components/SocialList.js
+++ src/components/SocialList.js
@@ -29,18 +29,20 @@ export const SocialList = ({ className, url, description }: Props) => {
   const facebookURL = `https://www.facebook.com/sharer/sharer.php?u=${e(url)}`
   const twitterURL = `https://www.twitter.com/intent/tweet?text=${e(tweetText)}`
 
-  return <ul className={`social-list ${className || ''}`}>
-    <li className='facebook link hint--bottom' data-hint='Share on Facebook'>
-      <a href={facebookURL} target='share'>
-        <span className='text'>F</span>
-      </a>
-    </li>
-    <li className='twitter link hint--bottom' data-hint='Share on Twitter'>
-      <a href={twitterURL} target='share'>
-        <span className='text'>T</span>
-      </a>
-    </li>
-  </ul>
+  return (
+    <ul className={`social-list ${className || ''}`}>
+      <li className='facebook link hint--bottom' data-hint='Share on Facebook'>
+        <a href={facebookURL} target='share'>
+          <span className='text'>F</span>
+        </a>
+      </li>
+      <li className='twitter link hint--bottom' data-hint='Share on Twitter'>
+        <a href={twitterURL} target='share'>
+          <span className='text'>T</span>
+        </a>
+      </li>
+    </ul>
+  )
 }
 
 export default SocialList
