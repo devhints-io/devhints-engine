128e30839ee5d1402f228c9cf6523df0c8ea3c50
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-09-20 21:40:38 +0800

    Omg postcss finally works, hallelujah

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     sheetPath: SHEET_PATH
   },
   plugins: [
+    'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-source-filesystem`,
--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,24 @@
 const DEBUG = process.env.NODE_ENV !== 'production'
 
-module.exports = {
-  plugins: {
-    'postcss-import': {},
-    // Workaround for https://github.com/zeit/styled-jsx/issues/366
-    'postcss-discard-comments': {},
+module.exports = ctx => {
+  return {
+    plugins: [
+      require('postcss-import')(),
 
-    'postcss-apply': {},
-    'postcss-preset-env': { stage: 0 },
+      // Workaround for https://github.com/zeit/styled-jsx/issues/366
+      require('postcss-discard-comments')(),
 
-    'postcss-browser-reporter': DEBUG && {},
-    'postcss-reporter': DEBUG && {}
+      require('postcss-apply')(),
+      require('postcss-preset-env')({
+        stage: 0,
+        preserve: false,
+        insertBefore: {
+          'all-property': require('postcss-color-mod-function')
+        }
+      }),
+
+      require('postcss-browser-reporter')(),
+      require('postcss-reporter')()
+    ]
   }
 }
