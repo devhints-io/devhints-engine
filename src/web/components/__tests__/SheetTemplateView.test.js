159afca7d41894761468b7f565201d69b1076853
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2018-09-23 15:43:12 +0800

    Fix test references

--- src/web/components/__tests__/SheetTemplateView.test.js
+++ src/web/components/__tests__/SheetTemplateView.test.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { mount } from 'enzyme'
 import SheetTemplateView from '../SheetTemplateView'
 import { Provider } from '../../contexts/SiteContext'
-import { CONTENT } from '../../../config'
+import { CONTENT } from '../../../../config'
 
 it.skip('works', () => {
   const props = {
