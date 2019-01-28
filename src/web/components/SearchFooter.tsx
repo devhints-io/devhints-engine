b6407a9d43a7bac74f85d523fbb259dc44db3a40
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-28 19:08:36 +0800

    Update SearchFooter to use CSS modules

--- src/web/components/SearchFooter.tsx
+++ src/web/components/SearchFooter.tsx
@@ -1,43 +1,17 @@
-/* @flow */
 import React from 'react'
-
-import HomeButton from './HomeButton'
-import SearchBox from '../../search/components/SearchBox'
+import CSS from './SearchFooter.module.css'
+import SearchFooterSection from './SearchFooterSection'
 
 /**
  * Search footer in the cheatsheets page.
  */
 
-export const SearchFooter = () => (
-  <aside className="search-footer" data-js-no-preview>
-    <div className="container">
+const SearchFooter = () => (
+  <aside className={CSS.root} data-js-no-preview>
+    <div className={CSS.container}>
       <SearchFooterSection />
     </div>
-
-    <style jsx>{`
-      @import 'src/web/styles/common';
-      @import 'src/web/styles/components/search-footer';
-    `}</style>
   </aside>
-)
-
-export const SearchFooterSection = () => (
-  <section className="search-footer-section">
-    <div className="search">
-      <form className="search" action="/" method="get">
-        <SearchBox />
-      </form>
-    </div>
-
-    <div className="links">
-      <HomeButton />
-    </div>
-
-    <style jsx>{`
-      @import 'src/web/styles/common';
-      @import 'src/web/styles/components/search-footer-container';
-    `}</style>
-  </section>
 )
 
 export default SearchFooter
