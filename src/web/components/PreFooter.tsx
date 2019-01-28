c54a4a8d25b0bd0a0b698a580d813da7f8a5fe51
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-01-28 19:12:18 +0800

    Update PreFooter to CSS modules

--- src/web/components/PreFooter.tsx
+++ src/web/components/PreFooter.tsx
@@ -1,32 +1,14 @@
-/* @flow */
 import React from 'react'
-import css from 'styled-jsx/css'
+import CSS from './PreFooter.module.css'
 
 /**
  * Pre-footer separator in the cheatsheets page
  */
 
 export const PreFooter = () => (
-  <div className="pre-footer">
-    <i className="icon" />
-
-    <style jsx>{STYLE}</style>
+  <div className={CSS.root}>
+    <i className={CSS.icon} />
   </div>
 )
-
-export const STYLE = css`
-  .pre-footer {
-    padding: 32px;
-    padding-top: 24px;
-    padding-bottom: 48px;
-    text-align: center;
-  }
-
-  .icon::before {
-    content: '';
-    /* TODO pre-footer icon -- @include ion-ios-flash(32px, $base-mute); */
-    opacity: 0.25;
-  }
-`
 
 export default PreFooter
