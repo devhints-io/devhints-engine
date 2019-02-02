5a37a6ee29a05b0e003e92b35dca198db6b1face
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-02-02 20:02:18 +0800

    Update some modules to .scss

--- src/web/components/BackButton.tsx
+++ src/web/components/BackButton.tsx
@@ -2,7 +2,7 @@ import cn from 'classnames'
 import { mdArrowBack as arrowLeft } from 'devhints-icons'
 import Link from 'gatsby-link'
 import React from 'react'
-import CSS from './BackButton.module.css'
+import CSS from './BackButton.module.scss'
 
 export const BackButton = () => {
   return (
--- src/web/components/PreFooter.tsx
+++ src/web/components/PreFooter.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CSS from './PreFooter.module.css'
+import CSS from './PreFooter.module.scss'
 
 /**
  * Pre-footer separator in the cheatsheets page
