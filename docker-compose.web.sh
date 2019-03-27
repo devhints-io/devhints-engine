d7a170e03d76c6ad68e9249c525c9081f502c5f9
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-03-27 20:57:07 +0800

    Add a docker-compose entrypoint

---
+++ docker-compose.web.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Docker-compose entry file
+
+# Install packages if necessary
+if [ ! -d node_modules ] || [ ! -x node_modules/.bin/gatsby ]; then
+  yarn
+fi
+
+# Ensure cache is cleared before running
+if [ -d .cache ]; then
+  rm -rf .cache/*
+fi
+
+# Start server
+yarn develop --host ${HOST:-0.0.0.0} --port ${PORT:-19336}
