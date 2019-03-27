321b4f539bde232fe74b7e51e06cbd73b2f296b5
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-03-27 19:25:37 +0800

    Fix gatsby-ssr.js warning

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,9 +1,11 @@
 // https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-plugin-styled-jsx/src/gatsby-ssr.js
 import flush from 'styled-jsx/server'
 
-exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+const onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   if (process.env.NODE_ENV === `production`) {
     const css = flush()
     setHeadComponents([css])
   }
 }
+
+export { onRenderBody }
