e09d205a1d0a6ca9174066488834955370e44966
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-05 21:33:41 +0800

    Refactor RelatedPostsCallout to use hooks

--- src/web/components/RelatedPostsCallout.tsx
+++ src/web/components/RelatedPostsCallout.tsx
@@ -1,4 +1,4 @@
-import Link from 'gatsby-link'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 import React from 'react'
 import CSS from './RelatedPostsCallout.module.scss'
 import SpanPushButton from './SpanPushButton'
@@ -7,18 +7,43 @@ export interface Props {
   pageCount: number
 }
 
-export const RelatedPostsCallout = ({ pageCount }: Props) => (
-  <span>
-    <Link className={CSS.root} to='/'>
-      <div className={CSS.text}>
-        <i className={CSS.icon} />
-        <span className={CSS.description}>
-          Over {pageCount} curated cheatsheets, by developers for developers.
-        </span>
-        <SpanPushButton className='isDark'>Devhints home</SpanPushButton>
-      </div>
-    </Link>
-  </span>
-)
+export const RelatedPostsCallout = ({ pageCount }: Props) => {
+  const {
+    site: {
+      siteMetadata: {
+        content: { relatedPostsCallout: content }
+      }
+    }
+  } = useStaticQuery(QUERY)
+
+  return (
+    <span>
+      <Link className={CSS.root} to='/'>
+        <div className={CSS.text}>
+          <i className={CSS.icon} />
+          <span className={CSS.description}>
+            {content.description.replace('SIZE', pageCount)}
+          </span>
+          <SpanPushButton dark>{content.link}</SpanPushButton>
+        </div>
+      </Link>
+    </span>
+  )
+}
+
+const QUERY = graphql`
+  {
+    site {
+      siteMetadata {
+        content {
+          relatedPostsCallout {
+            description
+            link
+          }
+        }
+      }
+    }
+  }
+`
 
 export default RelatedPostsCallout
--- src/web/components/SpanPushButton.tsx
+++ src/web/components/SpanPushButton.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import cn from 'classnames'
+import CSS from './SpanPushButton.module.scss'
 
 export interface Props {
   className?: string
+  dark?: boolean
   children?: React.ReactNode
 }
 
-const SpanPushButton = ({ className, children }: Props) => (
-  <span className={`push-button ${className || ''}`}>{children}</span>
-)
+const SpanPushButton = (props: Props) => {
+  const { className, children, isDark } = props
+
+  return (
+    <span
+      className={cn({
+        [CSS.root]: true,
+        [CSS.isDark]: props.dark,
+        [className]: true
+      })}
+    >
+      {children}
+    </span>
+  )
+}
 
 export default SpanPushButton
