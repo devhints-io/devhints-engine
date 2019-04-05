3fbd9a1c84661979283778e6618bcde9afb9be88
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-05 20:46:28 +0800

    Update LiveSearchInpot for hooks

--- src/search/containers/LiveSearchInput.tsx
+++ src/search/containers/LiveSearchInput.tsx
@@ -2,64 +2,69 @@
  * Put me anywhere for a cool search box.
  */
 
-import React from 'react'
+import React, { useState } from 'react'
 import { SiteSearchIndex } from '../../web/types'
 import SearchModal from '../components/SearchModal'
 import CSS from './LiveSearchInput.module.css'
 
-class LiveSearchInput extends React.Component<Props, State> {
-  state = {
-    isActivated: false,
-    initialValue: ''
-  }
+const DEFAULT_STATE = {
+  isActivated: false,
+  initialValue: ''
+}
 
-  render() {
-    const { props } = this
-    const { isActivated } = this.state
-    const { placeholder } = props
+/**
+ * Live search input
+ */
 
-    return (
-      <React.Fragment>
-        <input
-          type='text'
-          placeholder={placeholder || 'Search...'}
-          className={CSS.input}
-          onChange={this.handleInput}
-          value=''
+const LiveSearchInput = (props: Props) => {
+  const [state, setState] = useState(DEFAULT_STATE)
+  const { placeholder } = props
+
+  return (
+    <React.Fragment>
+      <input
+        type='text'
+        placeholder={placeholder || 'Search...'}
+        className={CSS.input}
+        onChange={handleInput.bind(null, { setState })}
+        value=''
+      />
+
+      {state.isActivated ? (
+        <SearchModal
+          siteSearchIndex={props.siteSearchIndex}
+          initialValue={state.initialValue}
+          onDismiss={dismissModal.bind(null, { setState })}
         />
+      ) : null}
+    </React.Fragment>
+  )
+}
 
-        {isActivated ? (
-          <SearchModal
-            siteSearchIndex={this.props.siteSearchIndex}
-            initialValue={this.state.initialValue}
-            onDismiss={this.dismissModal}
-          />
-        ) : null}
-      </React.Fragment>
-    )
-  }
+const handleInput = ({ setState }, event: { target: HTMLInputElement }) => {
+  const value = event.target.value
 
-  dismissModal = () => {
-    this.setState({ isActivated: false })
-  }
-
-  /**
-   * Activate the modal upon typing.
-   */
-
-  handleInput = (event: { target: HTMLInputElement }) => {
-    const value = event.target.value
-
-    if (value.trim().length) {
-      this.setState({ isActivated: true, initialValue: value })
-    }
+  if (value.trim().length) {
+    setState({ isActivated: true, initialValue: value })
   }
 }
+
+const dismissModal = ({ setState }) => {
+  setState({ isActivated: false })
+}
+
+/**
+ * Properties
+ */
 
 export interface Props {
   siteSearchIndex: SiteSearchIndex
   placeholder?: string
 }
+
+/**
+ * State
+ */
 
 export interface State {
   // This will be set to `true` when it's activated.
@@ -68,5 +73,9 @@ export interface State {
   // The initial value to be passed onto the modal dialog.
   initialValue: string
 }
+
+/**
+ * Export
+ */
 
 export default LiveSearchInput
