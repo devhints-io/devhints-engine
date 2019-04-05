a388521c3f2e5c91eccc5f721d87b74f1518be95
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-05 23:46:31 +0800

    Storybook: abort starting if node_modules aren't around yet

--- docker-compose.storybook.sh
+++ docker-compose.storybook.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
+if [ ! -x ./node_modules/.bin/start-storybook ]; then
+  echo "npm modules aren't installed yet."
+  echo "try bringing up the web service first"
+  exit 1
+fi
 
 # Yarn run
 yarn run start-storybook \
