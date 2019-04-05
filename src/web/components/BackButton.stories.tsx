7c73cfb6b06911aaa1ab36f70f5e4a7f58a91e43
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-05 23:49:10 +0800

    Add stub story for BackButton

---
+++ src/web/components/BackButton.stories.tsx
@@ -0,0 +1,7 @@
+import { storiesOf } from '@storybook/react'
+import React from 'react'
+import BackButton from './BackButton'
+
+storiesOf('BackButton', module).add('Default', () => {
+  return <BackButton />
+})
