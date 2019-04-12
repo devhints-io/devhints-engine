1dfef7bba38dd8b2aebfb961ffb10acd534cb9a5
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-12 21:51:57 +0800

    Implement combineContext()

--- src/penpad/index.tsx
+++ src/penpad/index.tsx
@@ -1,5 +1,15 @@
 export { default as Penpad } from './Penpad'
 
+type Result = any
+
+// A webpack context
+type WebpackContextFn = (id: string) => Result | { default?: Result }
+interface WebpackContext extends WebpackContextFn {
+  keys: () => string[]
+}
+
+// type WebpackContext = () => {})
+
 /**
  * Combines all objects in Webpack context into one object.
  * Useful for combining specimens.
@@ -8,8 +18,14 @@ export { default as Penpad } from './Penpad'
  *     combineContext(require.context('../', true, /\*.js/))
  */
 
-function combineContext(context: any): any {
-  return context.keys().reduce((result: any, item: any) => {
+function combineContext(context: WebpackContext | WebpackContext[]): any {
+  if (Array.isArray(context)) {
+    return context.reduce((result: any, ctx: WebpackContext) => {
+      return { ...result, ...combineContext(ctx) }
+    }, {})
+  }
+
+  return context.keys().reduce((result: Result, item: string) => {
     const exports = context(item)
     const specs = exports.default ? exports.default : exports
     return { ...result, ...specs }
