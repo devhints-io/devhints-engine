a1f085aaad9044ca580ea0341fbe50157d4747b7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-04-12 21:13:50 +0800

    Update

--- src/pages/styleguides.jsx
+++ src/pages/styleguides.jsx
@@ -1,1 +1,15 @@
-export { default } from '../styleguides'
+import React from 'react'
+import { Penpad } from '../penpad'
+
+const StyleguidePage = () => {
+  return (
+    <Penpad
+      pages={{}}
+      specimens={{
+        ...require('../styleguides/stuff.specimens.tsx').default
+      }}
+    />
+  )
+}
+
+export default StyleguidePage
