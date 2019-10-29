48cbf9c642327b5e4d90bd517220faf8db1bf7de
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-10-30 09:18:18 +1100

    Start to refactor css-base (~)

---
+++ src/css-base/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import 'sanitize.css/sanitize.css'
+import 'hint.css/hint.css'
+
+const CssBase = () => <></>
+export default CssBase
--- src/web/components/CommonHead.tsx
+++ src/web/components/CommonHead.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+
 import './CommonHead.css'
+
+import CssBase from '../../css-base'
 
 /**
  * Things that should be in all pages
  */
 
 export const CommonHead = () => (
-  <Helmet>
-    <link
-      rel='stylesheet'
-      href='https://fonts.googleapis.com/css?family=Cousine'
-    />
-  </Helmet>
+  <>
+    <Helmet>
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css?family=Cousine'
+      />
+    </Helmet>
+    <CssBase />
+  </>
 )
 
 export default CommonHead
