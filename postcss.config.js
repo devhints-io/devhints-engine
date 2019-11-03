e043e52e8cb7964a591c1bc56c925056844e0c18
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-11-03 18:26:08 +1100

    Use postcss-prepend-imports

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,18 @@ const DEBUG = process.env.NODE_ENV !== 'production'
 module.exports = ctx => {
   return {
     plugins: [
+      require('postcss-prepend-imports')({
+        files: [
+          require.resolve(
+            'responsive-modular-scale.css/responsive-modular-scale.css'
+          ),
+          require.resolve('./src/css-base/utils/gutter-padding.css'),
+          require.resolve('./src/css-base/utils/heading-style.css'),
+          require.resolve('./src/css-base/utils/section-gutter.css'),
+          require.resolve('./src/css-base/utils/has-container.css')
+        ]
+      }),
       require('postcss-import')(),
-
       require('postcss-apply')(),
       require('postcss-preset-env')({
         stage: 0,
