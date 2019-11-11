becdcbcbdf6629d0c6035939b167719a9e33477d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-11-11 22:04:36 +1100

    Add styled-jsx-plugin-postcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,9 +27,20 @@ module.exports = {
     'gatsby-plugin-typescript',
     'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-jsx',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-styled-jsx',
+      options: {
+        vendorPrefixes: false,
+        jsxPlugins: [
+          [
+            'styled-jsx-plugin-postcss',
+            { path: '[PATH_PREFIX]/postcss.config.js' }
+          ]
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: SHEET_PATH,
         name: 'markdown-pages'
