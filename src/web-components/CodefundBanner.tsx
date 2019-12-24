5322dc049e77df203ee4c770998cfcdfeee454bf
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2019-12-24 20:45:08 +1100

    Add async way of loading codefund ads

--- src/web-components/CodefundBanner.tsx
+++ src/web-components/CodefundBanner.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import css from 'styled-jsx/css'
 
 const CodefundBanner = () => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (!(window as any).abp) (window as any).abp = undefined
+    }
+  }, [])
+
+  useEffect(() => {
+    const rand = `${+new Date()}-${(Math.random() * 1000000) | 0}`
+    loadJs(`https://codefund.app/properties/51/funder.js?t=${rand}`, {
+      async: true
+    })
+    // loadJs(`https://cdn2.codefund.app/assets/px.js?ch=1&t=${rand}`, { async: false })
+    // loadJs(`https://cdn2.codefund.app/assets/px.js?ch=2&t=${rand}`, { async: false })
+  }, [])
+
   return (
     <div className='CodefundBox'>
       banner here
+      <div id='codefund'>{/* <!-- fallback content --> */}</div>
       <style jsx>{CSS}</style>
     </div>
   )
@@ -17,4 +33,12 @@ const CSS = css`
     text-align: center;
   }
 `
+
+const loadJs = (url: string, opts: { async: boolean }) => {
+  const tag = document.createElement('script')
+  tag.async = opts.async
+  tag.src = url
+  document.body.appendChild(tag)
+}
+
 export default CodefundBanner
